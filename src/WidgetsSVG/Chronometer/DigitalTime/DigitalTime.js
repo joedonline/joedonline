04f9d53a7d8076b81c3371c0839968c1c0e9c291
joedonline <contact@joedomingo.online>
2019-05-12 23:13:01 -0400

    Basic Chronometer mechanics set.

--- src/WidgetsSVG/Chronometer/DigitalTime/DigitalTime.js
+++ src/WidgetsSVG/Chronometer/DigitalTime/DigitalTime.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './DigitalTime.scss'
+import { setTime } from './timePassage'
 
 
-export default (props) => <i className="DigitalTime" style={{ color: 'white' }}>
-  <span>{ `SUN` }</span>
-  <span className="DigitalTime-now">{ `12:00` }</span>
-  <span>{ `AM` }</span>
-</i>
+export default (props) => {
+  useEffect( () => {
+    const dayofweek = document.getElementById('dayofweek')
+    const now = document.getElementById('now')
+    const meridian = document.getElementById('meridian')
+
+    setInterval(() => {
+      dayofweek.innerHTML = `${setTime().dayOfWeek}`
+      now.innerHTML = `${setTime().hour}:${setTime().minute}:${setTime().second}`
+      meridian.innerHTML = `${setTime().meridian}`
+    }, 1000)
+  } )
+
+  const passage = <>
+    <i className="DigitalTime" style={{ color: 'white' }}>
+      <div id="dayofweek"></div>
+      <div id="now" className="DigitalTime-now"></div>
+      <div id="meridian"></div>
+    </i>
+  </>
+
+  return passage
+}
---
+++ src/WidgetsSVG/Chronometer/DigitalTime/timePassage.js
@@ -0,0 +1,68 @@
+
+
+export const setTime = () => {
+  let d = new Date()
+  let h = d.getHours().toString()
+  let m = d.getMinutes().toString()
+  let s = d.getSeconds().toString()
+  let dayOfWeek = d.getDay()
+  const radix = 10
+
+  if (m.length===1) {
+    m = '0'.concat(m)
+  }
+
+  if (s.length===1) {
+    s = '0'.concat(s)
+  }
+
+  let timeOfDay
+  if (parseInt(h, radix)>20) {
+    timeOfDay = 'night'
+  } else if (parseInt(h, radix)>17) {
+    timeOfDay = 'evening'
+  } else if (parseInt(h, radix)>12) {
+    timeOfDay = 'afternoon'
+  } else if (parseInt(h, radix)===12) {
+    timeOfDay = 'noon'
+  } else if (parseInt(h, radix)>0 && parseInt(h, radix)<12) {
+    timeOfDay = 'morning'
+  } else if (parseInt(h, radix)===0) {
+    timeOfDay = 'midnight hour'
+  } else {
+    timeOfDay = '---'
+  }
+
+  let hour = 12 - parseInt(h, radix)
+  if (hour<0) hour *= -1
+  else {
+    hour = parseInt(h, radix)
+    if (hour===0) hour = 12
+  }
+
+  let hours = d.getHours()
+  let meridian = hours >= 12 ? 'pm' : 'am'
+
+  dayOfWeek = dayOfWeek === 0 ? 'sun' :
+              dayOfWeek === 1 ? 'mon' :
+              dayOfWeek === 2 ? 'tue' :
+              dayOfWeek === 3 ? 'wed' :
+              dayOfWeek === 4 ? 'thu' :
+              dayOfWeek === 5 ? 'fri' :
+              dayOfWeek === 6 ? 'sat'
+              : '---'
+
+  return {
+    hour: hour,
+    minute: m,
+    second: s,
+    timeOfDay: timeOfDay,
+    dayOfWeek: dayOfWeek,
+    meridian: meridian,
+    hours: hours
+  }
+
+}  // END setTime
+
+
+export const { hour, minute, second, timeOfDay, dayOfWeek, meridian, hours } = setTime()
