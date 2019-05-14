505993f63cfd50cf17aa0c3555bef545403cb1a3
joedonline <contact@joedomingo.online>
2019-05-14 17:27:53 -0400

    Chronometer refactor. Removed svg, replaced with pure css.

---
+++ src/WidgetsSVG/Chronometer/assembly/dial-hrdivs.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+
+export const hrdivs = [
+ <div id={`hour-12`} key={`hour-12`} className="hours" style={{ transform: `rotate(0deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-1`} key={`hour-1`} className="hours" style={{ transform: `rotate(30deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-2`} key={`hour-2`} className="hours" style={{ transform: `rotate(60deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-3`} key={`hour-3`} className="hours" style={{ transform: `rotate(90deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-4`} key={`hour-4`} className="hours" style={{ transform: `rotate(120deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-5`} key={`hour-5`} className="hours" style={{ transform: `rotate(150deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-6`} key={`hour-6`} className="hours" style={{ transform: `rotate(180deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-7`} key={`hour-7`} className="hours" style={{ transform: `rotate(210deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-8`} key={`hour-8`} className="hours" style={{ transform: `rotate(240deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-9`} key={`hour-9`} className="hours" style={{ transform: `rotate(270deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-10`} key={`hour-10`} className="hours" style={{ transform: `rotate(300deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>,
+ <div id={`hour-11`} key={`hour-11`} className="hours" style={{ transform: `rotate(330deg)` }}>
+   <div className="hours--ticker"></div>
+ </div>
+]
