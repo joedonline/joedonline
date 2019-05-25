438a3580801aa5432caa7f3cf4340dea3a67fbde
joedonline <contact@joedomingo.online>
2019-05-25 00:22:10 -0400

    Developer page, settings update.

--- src/Developer/Developer.js
+++ src/Developer/Developer.js
@@ -5,11 +5,7 @@ import Sidebar from './Sidebar/Sidebar'
 import Main from './Main/Main'
 
 
-export default (props) => {
-  window.scrollTo(0, 0)
-
-  return <>
-    <Sidebar />
-    <div className="Developer animated bounceInRight"><Main /></div>
-  </>
-}
+export default (props) => <>
+  <Sidebar />
+  <div className="Developer animated bounceInRight"><Main /></div>
+</>
