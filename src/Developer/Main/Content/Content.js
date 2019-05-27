3818a8b575909c4a7aeaf582fd5a56731bf19f2c
joedonline <contact@joedomingo.online>
2019-05-27 11:21:50 -0400

    Developer Page, content, responsive styling.

--- src/Developer/Main/Content/Content.js
+++ src/Developer/Main/Content/Content.js
@@ -15,21 +15,21 @@ export default (props) => {
   return <>
     <div className={`Content animated ${props.animation}`}>
       <h2 className={`Content-heading animated ${props.animation}`}>{ props.heading }</h2>
-        <div className={`contents animated ${props.animation} slow`}>
-          { props.isProjects
-            ? <div className="card-wrapper">
-                {
-                  props.content
-                  ? props.content.map(content => <article key={`K-${Math.floor(Math.random() * 9999)}-`.concat(`${Math.floor(Math.random() * 9999)}-EY`)} className="card">
-                        { contentHandler(content.acf.thumbnail, content.acf.title, content.acf.excerpt) }
-                      </article>
-                    )
-                  : <h1>{"loading..."}</h1>
-                }
-              </div>
-            : <div className={`contents animated ${props.animation} slow`} dangerouslySetInnerHTML={ { __html:  content } } />
-          }
-        </div>
+      <div className={`contents animated ${props.animation} slow`}>
+        { props.isProjects
+          ? <div className="card-wrapper">
+              {
+                props.content
+                ? props.content.map(content => <article key={`K-${Math.floor(Math.random() * 9999)}-`.concat(`${Math.floor(Math.random() * 9999)}-EY`)} className="card">
+                      { contentHandler(content.acf.thumbnail, content.acf.title, content.acf.excerpt) }
+                    </article>
+                  )
+                : <h1>{"loading..."}</h1>
+              }
+            </div>
+          : <div className={`contents animated ${props.animation} slow`} dangerouslySetInnerHTML={ { __html:  content } } />
+        }
+      </div>
     </div>
   </>
 }
