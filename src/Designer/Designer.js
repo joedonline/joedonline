e562c516caf9cc2a131bc7afc17467fb85b80b98
joedonline <contact@joedomingo.online>
2019-06-03 22:16:11 -0400

    Designer page, responsive styling adjustments.

--- src/Designer/Designer.js
+++ src/Designer/Designer.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import './Designer.scss'
 
+import { graphics } from '../DataFetch/graphics'
+
 
 export default (props) => {
-  return <div className="Designer animated fadeIn">
-           { `Coming soon.` }
-           <NavLink to="/">{ `back home` }</NavLink>
-         </div>
+  const [designerLogo, getDesignerLogo] = useState('')
+  const [homeLogo, getHomeLogo] = useState('')
+
+  useEffect(() => {
+    graphics().imageSource.then(imgData => {
+      if (imgData[2].acf) getDesignerLogo(imgData[2].acf.main_logo)
+      if (imgData[1].acf) getHomeLogo(imgData[1].acf.icon_home)
+    })
+  }, [])
+
+  const iconHomeStyles = {
+    background: `url(${homeLogo ? homeLogo : "#!"}) center/contain no-repeat`,
+  }
+
+  const logoStyles = {
+    display: 'block',
+    background: `url(${designerLogo ? designerLogo : "#!"}) center/contain no-repeat`,
+    width: '100%',
+    height: '25%'
+  }
+
+  return <>
+    <div className="Designer animated fadeIn">
+      <NavLink to="/" className="homelink" style={iconHomeStyles}></NavLink>
+      <div style={logoStyles} className="logoarea"></div>
+      <ul className="taglines">
+        <li>{ `graphic design` }</li>
+        <li>{ `motion graphics` }</li>
+        <li>{ `photography` }</li>
+        <li>{ `ui/ux` }</li>
+      </ul>
+      <NavLink to="/" className="portfoliolink">{ `designer portfolio` }</NavLink>
+      <div className="Designer-bgoverlay"></div>
+    </div>
+  </>
 }
